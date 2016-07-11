3fb5d6269c72e8e9cf566e9e992cbe4db550c1db
Alex Zimmerman <zimmerman@aices.rwth-aachen.de>
2016-07-11 17:54:32 +0200

    Compiles and runs. Didn't find parameter file

--- source/main.cc
+++ source/main.cc
@@ -1,6 +1,5 @@
 #include "pidomus.h"
 #include "interfaces/poisson_problem.h"
-#include "tests.h"
 
 using namespace dealii;
 int main (int argc, char *argv[])
@@ -8,16 +7,12 @@ int main (int argc, char *argv[])
 
   Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv,
                                                       numbers::invalid_unsigned_int);
-
-  initlog();
-  deallog.depth_file(1);
-
   const int dim = 2;
   const int spacedim = 3;
 
   PoissonProblem<dim,spacedim,LADealII> p;
   piDoMUS<dim,spacedim,LADealII> solver ("pidomus",p);
-  ParameterAcceptor::initialize(SOURCE_DIR "/parameters/poisson_problem_01.prm", "used_parameters.prm");
+  ParameterAcceptor::initialize("poisson_problem_01.prm", "used_parameters.prm");
 
 
   solver.run ();
