671806fa92e841f845fe65f83cf3217b1b38e5eb
Alex Zimmerman <zimmerman@aices.rwth-aachen.de>
2016-07-11 18:10:32 +0200

    User specified path to parameter input file.

--- source/main.cc
+++ source/main.cc
@@ -1,6 +1,6 @@
 #include "pidomus.h"
 #include "interfaces/poisson_problem.h"
-
+#include <string>
 using namespace dealii;
 int main (int argc, char *argv[])
 {
@@ -10,9 +10,17 @@ int main (int argc, char *argv[])
   const int dim = 2;
   const int spacedim = 3;
 
+  std::string parameter_filepath = "";
+  if (argc > 1) {
+    parameter_filepath = argv[1];
+	std::cout << "Using parameter input file at " << parameter_filepath << std::endl;
+  }
+  else {
+    std::cout << "Using default parameters." << std::endl;
+  }
   PoissonProblem<dim,spacedim,LADealII> p;
   piDoMUS<dim,spacedim,LADealII> solver ("pidomus",p);
-  ParameterAcceptor::initialize("poisson_problem_01.prm", "used_parameters.prm");
+  ParameterAcceptor::initialize(parameter_filepath, "used_parameters.prm");
 
 
   solver.run ();
