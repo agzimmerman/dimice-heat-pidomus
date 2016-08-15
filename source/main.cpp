519168c6331e2bfc02c8dcebfcc55460d38db551
Alex Zimmerman <zimmerman@aices.rwth-aachen.de>
2016-08-15 15:03:40 +0200

    Use interface with signals; use Trilinos

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,5 @@
 #include <pidomus.h>
-#include <interfaces/poisson_problem.h>
+#include "interfaces/poisson_problem_signals.h"
 #define _unused(x) ((void)(x)) // http://stackoverflow.com/questions/777261/avoiding-unused-variables-warnings-when-using-assert-in-a-release-build
 int main (int argc, char *argv[]) {
     const int dim = 2;
@@ -15,8 +15,8 @@ int main (int argc, char *argv[]) {
         parameter_file_path = exe_path.substr(0, exe_path.find_last_of("\\/"))+"/../inputs/sphere-cylinder.prm";
         std::cout << "Using default parameters at " << parameter_file_path << std::endl;
     }
-    PoissonProblem<dim,dim,LADealII> p;
-    piDoMUS<dim,dim,LADealII> solver("pi-DoMUS", p);
+    PoissonProblem<dim,dim,LATrilinos> p;
+    piDoMUS<dim,dim,LATrilinos> solver("pi-DoMUS", p);
     ParameterAcceptor::initialize(parameter_file_path, "used_parameters.prm");
     solver.run();
     _unused(mpi_init);
